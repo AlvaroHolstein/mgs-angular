177f7a0e5a9e51ac5e6d0095a211ab490984ee3d
alvaro <adholstein@gmail.com>
2019-04-05 22:09:39 +0100

    Added cards

--- readme.md
+++ readme.md
@@ -1,16 +1,18 @@
-#MGS site em Angular
-**O site vai estar a correr num servidor express**
+![MGS](http://www.mgserp.pt/images/pic01.jpg) ![Angular](http://www.w3bai.com/angular/pic_angular.jpg)
+
+#MGS site em Angular__
+**O site vai estar a correr num servidor express**__
 
 O site vai contar o numero de entradas que recebeu, 
 de momnento apenas está a contar o numero de entradas desde que o 
 servidor foi iniciado, mas consigo fazer com organizar o numero de 
 entradas por dia e depois guardar estes dados numa base de dados
 
-**Instalar o express**
+**Instalar o express**__
 `npm install`
 
-**Nodemon _(uma npm package util para desenvolvimento)_**
+**Nodemon _(uma npm package util para desenvolvimento)_**__
 `npm install -g nodemon`
 
-**Correr o servidor**
+**Correr o servidor**__
 `node server.js` _ou_ `nodemon server.js`
--- src/directives/jobaplication.directive.js
+++ src/directives/jobaplication.directive.js
@@ -5,7 +5,30 @@ app.directive('jobAplication', function () {
         templateUrl: './directives/jobaplication.directive.html',
 
         link: function (scope, element, attrs) {
+            scope.cards = [
+                {
+                    imgSrc: 'http://www.mgserp.pt/images/pic10.jpg',
+                    listItems: ['Graduation in Computer Science',
+                        'Strong experience with Javascript, HTML and CSS',
+                        'Frontend kills and rich experience with JS and JS frameworks/libs (design skills would be awesome)',
+                        'And most of all we need passion!'],
+                    title: 'What we need from you?' //Fazer filtro para upper case 
+                },
+                {
+                    imgSrc: 'http://www.mgserp.pt/images/pic11.jpg',
+                    listItems: ['Great work environment',
+                        'Build a career in an international company',
+                        'Training and working with a hard-working, dedicated and motivated team',
+                        'Our support enhancing your individual skills.'],
+                    title: 'What can we offer you?'
+                }
+            ]
 
+            scope.toogleForm = function () {
+
+                console.log('ata')
+                $('#jobForm').toggle();
+            }
         }
     }
 })
